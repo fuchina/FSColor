043c58505ca958e47037465ea5a03a6898e7ddb0
fudongdong <fudongdong@ihuman.com>
2023-12-19 12:15:29 +0800

    Update FSColor.m

--- FSColor/FSColor.m
+++ FSColor/FSColor.m
@@ -9,31 +9,31 @@
 
 @implementation FSColor
 
-+ (UIColor *)GreenColor_like_Wechat{
++ (UIColor *)GreenColor_like_Wechat {
     UIColor *color = [UIColor colorWithRed:0x20/255.0 green:0xbf/255.0 blue:0x66/255.0 alpha:1];
     return color;
 }
 
-+ (UIColor *)YellowColor_like_Meituan{
++ (UIColor *)YellowColor_like_Meituan {
     UIColor *color = [UIColor colorWithRed:1 green:195/255.0 blue:0 alpha:1];
     return color;
 }
 
-+ (UIColor *)RedColor_like_AppleNews{
++ (UIColor *)RedColor_like_AppleNews {
     UIColor *color = [UIColor colorWithRed:250 / 255.0 green:80/255.0 blue:100 /255.0 alpha:1];
     return color;
 }
 
-+ (UIColor *)BlueColor_like_Alipay{
++ (UIColor *)BlueColor_like_Alipay {
     UIColor *color = [UIColor colorWithRed:18 / 255.0 green:152/255.0 blue:233 /255.0 alpha:1];
     return color;
 }
 
-+ (UIColor *)BlueColor_like_WeChatFriends{
++ (UIColor *)BlueColor_like_WeChatFriends {
     return [UIColor colorWithRed:0x58/255.0 green:0x6c/255.0 blue:0x93/255.0 alpha:1];
 }
 
-+ (UIColor *)BlueColor_like_WeChatFriendsAlpha{
++ (UIColor *)BlueColor_like_WeChatFriendsAlpha {
     return [UIColor colorWithRed:0x58/255.0 green:0x6c/255.0 blue:0x93/255.0 alpha:.1];
 }
 
